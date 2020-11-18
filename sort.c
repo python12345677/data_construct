ec578b2fbd3d57f8cdad646da885d27cca36adf1
wr <827184674@qq.com>
2020-11-18 23:09:51 +0800

    three

--- sort.c
+++ sort.c
@@ -2,19 +2,21 @@
 #include<stdlib.h>
 
 int bubble_sort(int *p,int n);
+int choose_sort(int *p,int n);
+int insert_sort(int *p,int n);
 int tmp=0;
 int main()
 {
 	printf("hello wr\n");
 	int a[10];
 	int tmp=0;
-	srand(66);
+	srand(55);
 	for(int i=0;i<10;i++)
 	{
 		a[i]=rand()%100;
 		printf("%d ",a[i]);
 	}
-	bubble_sort(a,10);
+	choose_sort(a,10);
 	printf("\n");
 	for(int i=0;i<10;i++)
 	{
@@ -40,6 +42,43 @@ int bubble_sort(int *p,int n)
 
 int choose_sort(int *p,int n)
 {
-	int tmp=0;
+	int tmp=0,min_index=0;
+	for(int j=0;j<n-1;j++)
+	{
+		min_index=j;
+		for(int i=j+1;i<n;i++)
+		{
+			if(p[i]<p[min_index])
+			{
+				min_index=i;
+			}
+		}
+		tmp=p[j];p[j]=p[min_index];p[min_index]=tmp;
+	}
+	return 0;
+}
+
+int insert_sort(int *p,int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		int current=p[i+1];
+		for(int j=i;j>=0 ;j--)
+		{
+			if(p[j]>current)
+			{
+				p[j+1]=p[j];
+			}
+			else
+			{
+				p[j+1]=current;
+				break;
+			}
+			if(j==0)
+			{
+				p[j]=current;
+			}
+		}
+	}
 }
 
